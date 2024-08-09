61c1b2c8c59cc0bb6628ba570aa7097a19fdca03
Evian-Zhang <evianzhang1999@163.com>
2024-08-09 15:39:07 +0800

    Correctly take function address

--- tests/basic.rs
+++ tests/basic.rs
@@ -5,6 +5,6 @@ fn foo(a: usize) -> usize {
 #[test]
 fn basic_test() {
     let is_success =
-        unsafe { clear_cache::clear_cache(&foo, (&foo as *const _ as *const u8).add(4).cast()) };
+        unsafe { clear_cache::clear_cache(foo as *const (), (foo as *const ()).add(4)) };
     assert!(is_success)
 }
