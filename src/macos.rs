80db9ada2ba644199bd389897efd0d84c2d581d6
Evian-Zhang <evianzhang1999@163.com>
2024-08-09 15:27:01 +0800

    Fix CI bug

--- src/macos.rs
+++ src/macos.rs
@@ -1,7 +1,7 @@
 extern "C" {
     // libkern/OSCacheControl.h
     // void	sys_icache_invalidate( void *start, size_t len) __OSX_AVAILABLE_STARTING(__MAC_10_5, __IPHONE_2_0);
-    fn sys_icache_invalidate(start: *mut core::ffi::c_void, len: usize);
+    fn sys_icache_invalidate(start: *const core::ffi::c_void, len: usize);
 }
 
 pub(crate) unsafe fn os_arch_clear_cache<T>(start: *const T, end: *const T) -> bool {
