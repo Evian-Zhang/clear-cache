89bcc639de3c5c28db2b9a279720b2d306da0675
Evian-Zhang <evianzhang1999@163.com>
2024-11-27 18:24:29 +0800

    Get ready for Rust 2024 edition

--- src/macos.rs
+++ src/macos.rs
@@ -1,13 +1,15 @@
-extern "C" {
+unsafe extern "C" {
     // libkern/OSCacheControl.h
     // void	sys_icache_invalidate( void *start, size_t len) __OSX_AVAILABLE_STARTING(__MAC_10_5, __IPHONE_2_0);
     fn sys_icache_invalidate(start: *const core::ffi::c_void, len: usize);
 }
 
 pub(crate) unsafe fn os_arch_clear_cache<T>(start: *const T, end: *const T) -> bool {
-    sys_icache_invalidate(
-        start.cast(),
-        (end as *const u8).offset_from(start as *const u8) as usize,
-    );
+    unsafe {
+        sys_icache_invalidate(
+            start.cast(),
+            (end as *const u8).offset_from(start as *const u8) as usize,
+        );
+    }
     true
 }
